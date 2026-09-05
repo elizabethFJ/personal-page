54c2a244188a18f835aafb96900f6fc65f4f15c9
Elizabeth <floresjimeneze3@gmail.com>
2026-09-05 01:15:53 -0600

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -18,22 +18,3 @@ if (sections.length) {
   sections.forEach(section => observer.observe(section));
 }
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Llave pública: ABC123XYZ', 'success')
-  })
-}
